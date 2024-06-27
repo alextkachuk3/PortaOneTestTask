1e7e4579f8f01b1c8d1bdfb92ad5d9c09d4ce538
Oleksii Tkachuk <tkachuk.alexey@unicyb.kiev.ua>
2024-06-27 18:27:53 +0300

    Removed speed test comparsion

--- PortaOneTestTask/PortaOneTestTask.cpp
+++ PortaOneTestTask/PortaOneTestTask.cpp
@@ -1,7 +1,4 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <chrono>
 #include <fstream>
 #include <string>
 
@@ -10,39 +7,9 @@ bool isFileEmpty(std::ifstream& file)
     return file.peek() == std::ifstream::traits_type::eof();
 }
 
-void function1()
-{
-    FILE* file;
-
-    errno_t err;
-
-    err = fopen_s(&file, "10m.txt", "r");
-
-    char line[12];
-    int number;
-
-    if (file != NULL)
-    {
-        
-
-        while (fgets(line, sizeof(line), file))
-        {
-            number = std::atoi(line);
-            std::cout << number << std::endl;
-        }
-
-        fclose(file);
-    }
-    else
-    {
-        fprintf(stderr, "Unable to open file!\n");
-    }
-}
-
-int function2()
+int maxValue()
 {
     std::ifstream file("10m.txt");
-
 
     if (file.is_open()) 
     {
@@ -52,8 +19,10 @@ int function2()
             return 0;
         }
 
-        char line[12];
+        int numbersCount = 0;
         int number;
+        char line[12];
+        
 
         int maxElement = std::numeric_limits<int>::min();
                 
@@ -64,35 +33,24 @@ int function2()
              {
                  maxElement = number;
              }             
+             numbersCount++;
         }
 
         std::cout << maxElement << std::endl;
-        return maxElement;
 
         file.close();
+
+        return maxElement;        
     }
     else 
     {        
         std::cerr << "Unable to open file!" << std::endl;
+        return 0;
     }
 }
 
 int main()
 {
-    //// Measure execution time of function1
-    //auto start1 = std::chrono::high_resolution_clock::now();
-    //function1();
-    //auto end1 = std::chrono::high_resolution_clock::now();
-    //std::chrono::duration<double> duration1 = end1 - start1;
-    
-    // Measure execution time of function2
-    auto start2 = std::chrono::high_resolution_clock::now();
-    function2();
-    auto end2 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration2 = end2 - start2;
-
-    //std::cout << "Execution time of function1: " << duration1.count() << " seconds\n";
-    std::cout << "Execution time of function2: " << duration2.count() << " seconds\n";
-
+    maxValue();
     return 0;
 }
